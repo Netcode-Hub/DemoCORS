6142c7b4c3ab6b7930b4e3dd3275a9eda9339bc2
Netcode-Hub <Netcode-Hub@DESKTOP-ULDGKTU>
2024-05-13 06:21:14 +0000

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("BlazorApp", builder =>
+    {
+        builder.WithOrigins("https://localhost:7280")
+               .WithMethods("GET")
+               .AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseCors("BlazorApp");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BlazorApp2/Program.cs
@@ -0,0 +1,10 @@
+using BlazorApp2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7174/") });
+await builder.Build().RunAsync();
